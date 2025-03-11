74659eedb961c083d44b0fcf41cfdbd80da1dc16
ksasha-hub <vka2@list.ru>
2025-03-11 13:03:48 +0000

    Программа, которая вычисляет параметры прямоугольника

---
+++ kvadrat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Функция для проверки, что числа положительные
+bool isPositive(double a, double b) {
+    return a > 0 && b > 0;
+}
+
+// Функция для вычисления периметра прямоугольника
+double rectanglePerimeter(double a, double b) {
+    return 2 * (a + b);
+}
+
+// Функция для вычисления площади прямоугольника
+double rectangleArea(double a, double b) {
+    return a * b;
+}
+
+// Функция для вычисления длины диагонали прямоугольника
+double rectangleDiagonal(double a, double b) {
+    return sqrt(a * a + b * b);
+}
