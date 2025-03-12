45258ccb08e7c91d10ec91766483d63601a4696a
danilglokbog <danilglok24@gmail.com>
2025-03-12 12:09:10 +0700

    delete

--- triangle.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRIANGLE_H
-#define TRIANGLE_H
-
-double trianglePerimeter(double a, double b, double c);
-double triangleArea(double a, double b, double c);
-bool isIsosceles(double a, double b, double c);
-
-#endif // TRIANGLE_H
