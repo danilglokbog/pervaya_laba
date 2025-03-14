91adf92dff90c96fdaa01d3b2cbcf2445c555735
danilglokbog <danilglok24@gmail.com>
2025-03-14 09:14:33 +0700

    upt

---
+++ kvad.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+        setlocale(LC_ALL, "Russian");
+        double a,b,c;
+        cout << "Введите сторону a: "; // Пользователь вводит стороны прямоугольника
+        cin >> a; 
+        cout << "Введите сторону b: ";
+        cin >> b;
+        if (a > 0 && b > 0) { // Проверка, что стороны положительны 
+                double p = (a + b) * 2; // Вычисление периметра
+                double s = a * b; // Вычисление площади
+                double d = sqrt(a * a + b * b); //  Вычисление длины диагонали
+                cout << "Perimeter: "<< p << endl << "Area: " << s << endl << "Diagonal: " << d << endl;
+        }
+        else {
+                cout << "ERR" << endl; // Если не положительны, то ошибка
+        }
+return 0;
+}
